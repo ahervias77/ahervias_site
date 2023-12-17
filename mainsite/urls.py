1ad247ce7534f1d96f7fbb46f358220794050ce3
Austin Hervias <19595272+ahervias77@users.noreply.github.com>
2023-12-16 22:43:56 -0600

    Add resume and projects pages

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from mainsite.views import index
+from mainsite.views import index, resume, projects
 
 app_name = 'mainsite'
 urlpatterns = [
     path('', index, name='index'),
+    path('resume', resume, name='resume'),
+    path('projects', projects, name='projects'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainsite/index.html')
+
+def resume(request):
+    return render(request, 'mainsite/resume.html')
+
+def projects(request):
+    return render(request, 'mainsite/projects.html')
