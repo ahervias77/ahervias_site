eb75a75314fbd5367aa61b22e5f8da470deef0c0
Austin Hervias <19595272+ahervias77@users.noreply.github.com>
2023-12-17 14:28:50 -0600

    Security enhancements to settings.py

--- ahervias_site/settings.py
+++ ahervias_site/settings.py
@@ -21,17 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'dev')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l@&)-oyvsog$*ctgf=*gav_dut9plbzz6v0dn8xh4t)#h2@f5u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l@&)-oyvsog$*ctgf=*gav_dut9plbzz6v0dn8xh4t)#h2@f5u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '.vercel.app',
-    '.ahervias.dev'
-]
+if ENVIRONMENT != 'dev':
+    DEBUG = False
+    ALLOWED_HOSTS = ['localhost', '.vercel.app', '.ahervias.dev']
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = ['*', '0.0.0.0']
 
 
 # Application definition
