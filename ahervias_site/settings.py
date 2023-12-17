7b3a6840aecab7ac9a140d737b57556a83d67df5
Austin Hervias <19595272+ahervias77@users.noreply.github.com>
2023-12-16 19:56:25 -0600

    Add Vercel domain to allowed hosts in settings.py

--- ahervias_site/settings.py
+++ ahervias_site/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-l@&)-oyvsog$*ctgf=*gav_dut9plbzz6v0dn8xh4t)#h2@f5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '.vercel.app'
+]
 
 
 # Application definition
